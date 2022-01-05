02af42916654d1f3af16d1c51142dd5af7487d98
Julius Kirsch <72889252+FrozenAssassine@users.noreply.github.com>
2022-01-05 14:10:25 +0100

    Update WS2812.h

--- WS2812.h
+++ WS2812.h
@@ -123,47 +123,15 @@ public:
 			leditems[i].blue = blue;
 		}
 	}
-	uint32_t HSVtoRGB(uint16_t H, uint8_t S,uint8_t V){
-		if(H>360 || H<0 || S>100 || S<0 || V>100 || V<0){
-			//Invalid input
-			return 0;
-		}
-	
-		float s = S/100;
-		float v = V/100;
-		float C = s*v;
-		float X = C*(1-abs(fmod(H/60.0, 2)-1));
-		float m = v-C;
-		float r,g,b;
-		if(H >= 0 && H < 60){
-			r = C,g = X,b = 0;
-		}
-		else if(H >= 60 && H < 120){
-			r = X,g = C,b = 0;
-		}
-		else if(H >= 120 && H < 180){
-			r = 0,g = C,b = X;
-		}
-		else if(H >= 180 && H < 240){
-			r = 0,g = X,b = C;
-		}
-		else if(H >= 240 && H < 300){
-			r = X,g = 0,b = C;
-		}
-		else{
-			r = C,g = 0,b = X;
-		}
-		uint32_t rgb = (r+m)*255;
-		rgb = (rgb << 8) + (g+m)*255;
-		rgb = (rgb << 8) + (b+m)*255;
-		return rgb;
-	}
 	void InitialiseStrip()
 	{
 		for (uint8_t i = 0; i < ArraySize(leditems); i++) {
 			leditems[i].red = leditems[i].green = leditems[i].blue =0;
+			for(uint8_t i = 0; i<24;i++){
+				TimeLed(0);
+			}
+			_delay_us(50);
 		}
-		ShowPixel();
 	}
 	uint16_t GetLedCount()
 	{
