59f5929b47b6f34c78e60f55101eb880c390daa9
Julius Kirsch <72889252+FrozenAssassine@users.noreply.github.com>
2022-08-13 10:07:14 +0200

    Update main.cpp

--- examples/ManualColor/main.cpp
+++ examples/ManualColor/main.cpp
@@ -1,19 +1,18 @@
-#define NumberOfPixels 12 //Set the number of leds
 #define F_CPU 8000000 //Set the clockspeed
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdbool.h>
-#include <WS2812.h> //include the libary
+#include "WS2812.h"
 
 int main(void)
 {
 	DDRB = 0b00000001; //Define the output
 	
-	InitialiseStrip();
+	Strip strip = Strip(12, PORTB, PINB0);	
+	strip.InitialiseStrip(); //Turn all leds off
 
-	SetPixelColor(11, 255, 0, 0); //Set color of pixel 11 to red
-	SetPixelColor(3, 5 255, 255, 0); //Set color of pixel 3 to 8 green
-	SetPixelColor(5, 65025); //Set color as 32bit value: for example 65025 for green
-	ShowPixel(); //Call to actually display the changes
+	strip.SetPixelColor(11, 255, 0, 0); //Set color of pixel 11 to red
+	strip.SetPixelColor(3, 5 255, 255, 0); //Set color of pixel 3 to 8 green
+	strip.SetPixelColor(5, 65025); //Set color as 32bit value: for example 65025 for green
+	strip.ShowPixel(); //Call to actually display the changes
 }
 	
